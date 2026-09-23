27c9cb6651ab1794236ab5c166fb25690b5acfe1
leonardoadame837-png <leonardoadame837@gmail.com>
2026-09-23 16:50:58 -0700

    Make Windows Gradle wrapper launcher safe when wrapper JAR is absent

--- gradlew.bat
+++ gradlew.bat
@@ -3,8 +3,17 @@
 setlocal
 set DIR=%~dp0
 set CLASSPATH=%DIR%gradle\wrapper\gradle-wrapper.jar
-if not exist "%CLASSPATH%" (
-  echo Gradle wrapper JAR not found: %CLASSPATH%
-  exit /b 1
+
+if exist "%CLASSPATH%" (
+  java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+  exit /b %ERRORLEVEL%
 )
-java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+where gradle >nul 2>&1
+if %ERRORLEVEL% EQU 0 (
+  gradle %*
+  exit /b %ERRORLEVEL%
+)
+
+echo Gradle wrapper JAR is missing and no system Gradle installation was found.
+exit /b 1
